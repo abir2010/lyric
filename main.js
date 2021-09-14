12ce54c37edf57bdc00db61b13feb1f60693d77d
unknown <kaiserabir222@gmail.com>
2021-09-14 21:00:47 +0600

    lyric part added

--- main.js
+++ main.js
@@ -22,11 +22,27 @@ const displayLyric = (data) => {
             <p class="author lead">Album by <span class="fw-bold">${data[i].artist.name}</span></p>
         </div>
         <div class="col-md-3 col-3 text-md-right text-center">
-            <button onclick="getlyric(${data[i].artist.name},${data[i].title})" class="btn btn-success">Get Lyrics</button>
+            <button onclick="getlyric('${data[i].artist.name}','${data[i].title}')" class="btn btn-success">Get Lyrics</button>
         </div>`;
         displayLyrics.appendChild(div);
     }
 }
 const getlyric = (artist,title) => {
-    console.log(artist,title);
+    const url2 = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    fetch(url2)
+        .then(res => res.json())
+        .then(data => lyrics(data))
+}
+const lyrics = data => {
+    const displayLyrics = document.getElementById('display-lyric');
+    displayLyrics.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('lyric-result','row','align-items-center','my-3','py-3');
+    const l = data.lyrics;
+    div.innerHTML = `
+    <div class="text-center px-5">
+        <p>${l.slice(21,100000)}</p>
+    </div>
+    `
+    displayLyrics.appendChild(div);
 }
