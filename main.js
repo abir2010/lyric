46febbed505493fa7c1e5c8531029b1fff170931
unknown <kaiserabir222@gmail.com>
2021-09-14 21:30:03 +0600

    almost done

--- main.js
+++ main.js
@@ -9,40 +9,43 @@ const showLyric = () => {
 }
 const displayLyric = (data) => {
     const displayLyrics = document.getElementById('display-lyric');
-    displayLyrics.textContent = '';
-    for(let i=0; i<data.length; i++){
-        const div = document.createElement('div');
-        div.classList.add('single-result','row','align-items-center','my-3','py-3')
-        div.innerHTML = `
-        <div class="col-md-2 col-4">
-            <img class="img-fluid rounded-3" src="${data[i].album.cover_medium}">
-        </div>
-        <div class="col-md-7 col-5">
-            <h4 class="lyrics-name">${data[i].title}</h4>
-            <p class="author lead">Album by <span class="fw-bold">${data[i].artist.name}</span></p>
-        </div>
-        <div class="col-md-3 col-3 text-md-right text-center">
-            <button onclick="getlyric('${data[i].artist.name}','${data[i].title}')" class="btn btn-success">Get Lyrics</button>
-        </div>`;
-        displayLyrics.appendChild(div);
+    if(displayLyrics != null || data){
+        displayLyrics.textContent = '';
+        for(let i=0; i<data.length; i++){
+            const div = document.createElement('div');
+            div.classList.add('single-result','row','align-items-center','my-3','py-3')
+            div.innerHTML = `
+            <div class="col-md-2 col-4">
+                <img class="img-fluid rounded-3" src="${data[i].album.cover_medium}">
+            </div>
+            <div class="col-md-7 col-5">
+                <h4 class="lyrics-name">${data[i].title}</h4>
+                <p class="author lead">Album by <span class="fw-bold">${data[i].artist.name}</span></p>
+            </div>
+            <div class="col-md-3 col-3 text-md-right text-center">
+                <button onclick="getlyric('${data[i].artist.name}','${data[i].title}')" class="btn btn-success">Get Lyrics</button>
+            </div>`;
+            displayLyrics.appendChild(div);
+        }
     }
 }
 const getlyric = (artist,title) => {
     const url2 = `https://api.lyrics.ovh/v1/${artist}/${title}`;
     fetch(url2)
         .then(res => res.json())
-        .then(data => lyrics(data))
+        .then(data => lyrics(data,artist,title))
 }
-const lyrics = data => {
+const lyrics = (data,artist,title) => {
     const displayLyrics = document.getElementById('display-lyric');
     displayLyrics.textContent = '';
     const div = document.createElement('div');
     div.classList.add('lyric-result','row','align-items-center','my-3','py-3');
-    const l = data.lyrics;
     div.innerHTML = `
     <div class="text-center px-5">
-        <p>${l.slice(21,100000)}</p>
+        <h3>${title}</h3>
+        <h5>By ${artist}</h5>
+        <p>${data.lyrics}</p>
     </div>
-    `
+    `;
     displayLyrics.appendChild(div);
 }
